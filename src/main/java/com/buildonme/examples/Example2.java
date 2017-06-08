b123b32221c573cc4a460f8acb9c43030df04241
Alex <alex.povodyrev@gmail.com>
2017-06-07 21:37:43 -0400

    modified example 2

--- src/main/java/com/buildonme/examples/Example2.java
+++ src/main/java/com/buildonme/examples/Example2.java
@@ -18,6 +18,7 @@ import java.util.List;
 public class Example2 {
 //    private static Logger log = Logger.getLogger(Example2.class);
 
+
     public static void main(String [] args){
 
         //Setup
@@ -27,25 +28,98 @@ public class Example2 {
 
 
 
-        List<Document> boroughs = collection.aggregate(
+
+        List<Document> boroughs1 = collection.aggregate(
                 Arrays.asList(
-                        Aggregates.match(Filters.eq("cuisine", "Bakery")),
+//                        Aggregates.match(Filters.eq("cuisine", "Bakery")),
+                        Aggregates.match(new Document("cuisine", "Bakery")),
                         Aggregates.group("$borough", Accumulators.sum("count", 1))
                 )
         ).into(new ArrayList<Document>());
 
-        for(Document d:boroughs){
+        for(Document d:boroughs1){
             System.out.println(d);
         }
 
+        //List of bakeries
+        List<Document> bakeries = collection.find( new Document("cuisine", "Bakery") ).into(new ArrayList<Document>());
+
+        //List of bakeries in manhattan
+        List<Document> bakeriesInManhattan = new ArrayList<Document>();
+
+        // Populate bakeriesInManhattan
+        int totalCount = 0;
+        int manhattanCount = 0;
+
+        for (Document d : bakeries){
+
+            String borough = d.getString("borough");
+
+            if (borough.equals("Manhattan")) {
+                bakeriesInManhattan.add(d);
+                manhattanCount++;
+            }
+
+            totalCount++;
+        }
+
+        // Print stats
+        System.out.println("Went through " + String.valueOf(totalCount) + " records.");
+        System.out.println("Added " + String.valueOf(manhattanCount));
 
 
-        // Into List (using filter)
-        List<Document> restaurants = collection.find(
-                new Document("borough", "Bronx").append("cuisine", "Bakery") //Filter Bakeries in bronx
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Filter Bakeries in bronx that have recieved a score above 50
+        List<Document> restaurants1 = collection.find(
+
+                new Document("borough", "Bronx")
+                .append("cuisine", "Bakery")
+                .append("grades.score", new Document("$gte", 50))
+
         ).into(new ArrayList<Document>());
 
-        System.out.println(restaurants.size());
+        //System.out.println(restaurants1.size());
+
+//        System.out.println(boroughs.get(0).getString("_id"));
+
+//        List<List<Document>> boroughs = new ArrayList<List<Document>>();
+
+
+
+
+//        List<Document> boroughs = new ArrayList<Document>();
+//
+//        for ( Document d : boroughs1){
+//            String boroughName = d.getString("_id");
+//            boroughs.add(  new Document( boroughName, new Document("1", 3) )  );
+//        }
+//
+//        System.out.println(boroughs);
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
